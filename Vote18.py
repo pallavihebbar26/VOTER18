bb066f38f663c219ab52c45f0054ae529c34278f
Manjari <01fe24ica060kletech.ac.in>
2025-11-27 21:27:11 +0530

    second commit

--- Vote18.py
+++ Vote18.py
@@ -4,4 +4,4 @@ age = int(input("Enter your age: "))
 if age >= 18:
     print("You are eligible to vote...")
 else:
-    print("You are not eligible to vote yet....")
+    print("You are not eligible to vote to yet....")
